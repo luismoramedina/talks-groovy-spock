0a772d326421d4b456fd4d68fd82a6cfadd69569
Javier Sanz <javier.sanz.salesa@gmail.com>
2015-02-04 12:47:57 +0100

    Add console debug traces that verify passed tests

--- src/main/groovy/test-runner.groovy
+++ src/main/groovy/test-runner.groovy
@@ -1,26 +1,36 @@
 filmService = new FilmServiceImpl()
+println "[DEBUG] Test carga fichero OK"
 
 //THE FILM
 theFilm = filmService.getFilm(1)
 assert theFilm.name.equalsIgnoreCase("Blade runner")
+println "[DEBUG] Test getFilm OK"
 
 //Director -> Fincher
 filmsByFincher = filmService.getFilmsByDirector("David Fincher")
 assert filmsByFincher.size() == 1
 assert filmsByFincher.get(0).name == "Fight club"
+println "[DEBUG] Test getFilmsByDirector OK"
 
 //Actor -> Bale (AKA The Batman before Ben Affleck)
 baleFilms = filmService.getFilmsByStar("Christian Bale")
 assert baleFilms.size() == 2
+println "[DEBUG] Test getFilmsByStar OK"
 
 //Top films
 nineStarsFilms = filmService.getFilmsByRanking(9)
 assert nineStarsFilms.size() == 3
+println "[DEBUG] Test getFilmsByRanking equals OK"
 nineOrMoreStarsFilms = filmService.getFilmsByRanking(9, true)
 assert nineOrMoreStarsFilms.size() == 4
+println "[DEBUG] Test getFilmsByRanking equalsOrMore OK"
 nineOrLessStarsFilms = filmService.getFilmsByRanking(8, false, true)
 assert nineOrLessStarsFilms.size() == 1
+println "[DEBUG] Test getFilmsByRanking equalsOrLess OK"
 
 //All films in database
 allFilms = filmService.getFilms()
 assert allFilms.size() == 5
+println "[DEBUG] Test getFilms OK"
+
+println "[DEBUG] All Tests OK!!!!!"
